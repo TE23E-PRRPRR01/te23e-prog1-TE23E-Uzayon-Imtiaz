e12a085ca65de610686c971f4c0bf5c233e9ad1e
Uzayon Imtiaz <uzayon.imtiaz@elev.ga.ntig.se>
2024-08-26 14:45:06 +0200

    Tränat lite på if satser.

--- Kapitel 1/Regristrering/Program.cs
+++ Kapitel 1/Regristrering/Program.cs
@@ -9,3 +9,15 @@ Console.WriteLine("Ange efternamn: ");
 string Efternamn = Console.ReadLine();
 Console.WriteLine("Hej! " + Förnamn + " " + Efternamn);
 Console.WriteLine($"Hej! {Förnamn} {Efternamn} ");
+
+// Är det Uzayon som loggar in
+if (Förnamn == "Uzayon")
+{
+    Console.WriteLine("Vad trevligt att du är tillbacka!");
+}
+
+// Är det Imtiaz som loggar in
+if (Efternamn == "Imtiaz")
+{
+    Console.WriteLine("Hello Mr.Imtiaz!");
+}
