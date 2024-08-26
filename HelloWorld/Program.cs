0217386df9d1d32e437e5de47f94955dd1c00c13
Uzayon Imtiaz <uzayon.imtiaz@elev.ga.ntig.se>
2024-08-26 14:31:44 +0200

    Tränat på cw, cr och svenska tecken.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Hello, Quandale Dingle!");
+Console.WriteLine("Wazz Upppp");
+Console.WriteLine("Yo mama!");
+Console.WriteLine("Nahh bruv");
+Console.WriteLine("Como te llamas me amigo?");
+string namn = Console.ReadLine();
+Console.WriteLine("Me llamo " + namn);
+Console.WriteLine("Age check?");
+string Ålder = Console.ReadLine();
+Console.WriteLine("Dawg I'm " + Ålder);
+
